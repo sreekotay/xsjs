10794283a27ae8907dae5a84c1cedcf5145f5e9d
Sree Kotay <sreekotay@gmail.com>
2019-04-05 19:20:10 -0400

    cookie fixes

--- xs_utils.js
+++ xs_utils.js
@@ -56,38 +56,27 @@ function xs_GUIDGen() {
 
 
 
-function xs_cookie(cname, cvalue, exMS) {
-    //set
-    if (cvalue===null) {
-      document.cookie = cname+'=; Max-Age=-99999999;';
-      return null;
+function setCookie(name,value,ms) {
+    var expires = "";
+    if (ms) {
+        var date = new Date();
+        date.setTime(date.getTime() + ms);
+        expires = "; expires=" + date.toUTCString();
     }
-    if (cvalue!==undefined) {
-        var expires = ''
-        if (exMS) {
-            var d = new Date();
-            d.setTime(d.getTime() + exMS);
-            expires = ";expires="+ d.toUTCString();
-        }
-        document.cookie = cname + "=" + encodeURIComponent(cvalue) + expires + ";path=/";
-        return cvalue;
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
-    //get
-    try {
-      var name = cname + "=";
-      var decodedCookie = decodeURIComponent(document.cookie);
-      var ca = decodedCookie.split(';');
-      for(var i = 0; i <ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == ' ')
-              c = c.substring(1);
-          if (c.indexOf(name) == 0)
-              return c.substring(name.length, c.length);
-      }
-    } catch (err) {
-      console.log(err)
-    }
-    return "";
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
 }
 
     
